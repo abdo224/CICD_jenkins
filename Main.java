de3f5b296fb46e8c7d773fe4054462884d70219c
abdo224 <ibenaziza@inttic.dz>
2022-09-04 17:11:14 +0100

    new commit

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello from java , because python did not work for me !!!!");
+        System.out.println("hello from java , ");
     }
 }
