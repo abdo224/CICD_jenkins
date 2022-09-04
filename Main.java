e887283aaf80346d804472e193fff35ee9b647ec
abdo224 <ibenaziza@inttic.dz>
2022-09-04 17:15:10 +0100

    new commit

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello from java , ");
+        System.out.println("hello from java , sdfjjkxdg");
     }
 }
